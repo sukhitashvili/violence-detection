663f6f7ad390200aba05c7793162ef9d14bdcf6b
sukhitashvili <sukhitashvili.soso@gmail.com>
2021-10-05 15:12:59 +0400

    added processing code

--- processor.py
+++ processor.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 import yaml
 from PIL import Image
+import logging
 
 
 class Model:
@@ -14,6 +15,7 @@ class Model:
 
         self.device = self.settings['model-settings']['device']
         self.model_name = self.settings['model-settings']['model-name']
+        self.threshold = self.settings['model-settings']['prediction-threshold']
         self.model, self.preprocess = clip.load(self.model_name,
                                                 device=self.device)
         self.labels = self.settings['label-settings']['labels']
@@ -22,7 +24,7 @@ class Model:
             text = 'a photo of ' + label  # will increase model's accuracy
             self.labels_.append(text)
 
-        text_features = self.get_text_vector(self.labels_)
+        self.text_features = self.vectorize_text(self.labels_)
         self.default_label = self.settings['label-settings']['default-label']
 
     @torch.no_grad()
@@ -32,20 +34,43 @@ class Model:
         return tf_image
 
     @torch.no_grad()
-    def get_text_vector(self, text: list):
+    def tokenize(self, text: list):
         text = clip.tokenize(text).to(self.device)
         return text
 
     @torch.no_grad()
-    def predict_label(self, image: np.array):
-        tf_image = self.tranform_image(image)
-        logits_per_image, logits_per_text = model(tf_image, text_features)
-        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-        label_index = probs.argmax(axis=1).item()
-        label_text = self.labels[label_index]
+    def vectorize_text(self, text: list):
+        tokens = self.tokenize(text=text)
+        text_features = self.model.encode_text(tokens)
+        return text_features
+
+    @torch.no_grad()
+    def predict_(self, text_features: torch.Tensor,
+                 image_features: torch.Tensor):
+        # Pick the top 5 most similar labels for the image
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+        similarity = image_features @ text_features.T
+        values, indices = similarity[0].topk(1)
+        return values, indices
+
+    @torch.no_grad()
+    def predict(self, image: np.array) -> str:
+        tf_image = self.transform_image(image)
+        image_features = self.model.encode_image(tf_image)
+        values, indices = self.predict_(text_features=self.text_features,
+                                        image_features=image_features)
+        label_index = indices[0].cpu().item()
+        label_text = self.default_label
+        model_confidance = abs(values[0].cpu().item())
+        if model_confidance >= self.threshold:
+            label_text = self.labels[label_index]
+
+        print(f'predicted label is "{label_text}" with confidence of: {model_confidance}')
         return label_text
 
-    def plot_image(self, image: np.array, title_text: str):
+    @staticmethod
+    def plot_image(image: np.array, title_text: str):
         plt.figure(figsize=[13, 13])
         plt.title(title_text)
         plt.axis('off')
