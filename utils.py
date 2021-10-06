3cfdddc587c5aba593b5728f3bda878f2695e9df
sukhitashvili <sukhitashvili.soso@gmail.com>
2021-10-06 15:01:01 +0400

    added title size param

--- utils.py
+++ utils.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot(image: np.array, figsize: tuple = (13, 13),
+def plot(image: np.array, title: str, title_size: int = 16,
+         figsize: tuple = (13, 13),
          convert_to_rgb: bool = True):
     if convert_to_rgb:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     plt.figure(figsize=figsize)
+    plt.title(title, size=title_size)
     plt.axis('off')
     plt.imshow(image)
