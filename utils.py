02119bd6ca4e15e34696ae2ca7284802859653e9
sukhitashvili <sukhitashvili.soso@gmail.com>
2021-10-07 18:41:09 +0400

    removed to RGB convertion

--- utils.py
+++ utils.py
@@ -1,16 +1,12 @@
 from typing import Union
 
-import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def plot(image: np.array, title: str, title_size: int = 16,
          figsize: tuple = (13, 7),
-         convert_to_rgb: bool = True,
          save_path: Union[str, None] = None):
-    if convert_to_rgb:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     plt.figure(figsize=figsize)
     plt.title(title, size=title_size)
     plt.axis('off')
